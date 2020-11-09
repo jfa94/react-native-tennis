bb3b264a1848d68a5001bc9566c7756dda5fd08b
Javier <j.flores.almagro@gmail.com>
2020-11-09 08:08:01 +0000

    Initial commit

---
+++ DummyData/ARTICLES.js
@@ -0,0 +1,17 @@
+export default {
+  "1": {
+    "title": "Somethings happened to Player",
+    "description": "Click on this article to find out what happened",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/8/8e/Roger_Federer_2012_Indian_Wells.jpg"
+  },
+  "2": {
+    "title": "Check out this news article",
+    "description": "There is an update that you should probably have a look at",
+    "image": "https://cdn.images.express.co.uk/img/dynamic/72/590x/1357460_1.jpg"
+  },
+  "3": {
+    "title": "You'll never believe what this person just said",
+    "description": "It was honestly bananas, you should totally read the hot take we wrote",
+    "image": "https://static01.nyt.com/images/2020/08/28/sports/28tennis-players01/28tennis-players01-videoSixteenByNineJumbo1600.jpg"
+  }
+}
